fd141194c6622c62c6e741107c96750195116745
Abián <70582564+abian22@users.noreply.github.com>
2024-01-24 12:14:32 +0000

    nightmode done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import "./App.scss";
 import { useState } from "react";
 import { useThemeContext } from "./context/ThemeContext";
 import ReactSwitch from "react-switch";
-import foto4 from "./assets/images/pizza.png";
+import darkPizza from "./assets/images/darkMode pizza.png";
+import lightPizza from "./assets/images/lightMode pizza.png";
+
 
 function App() {
   const { contextTheme, setContextTheme } = useThemeContext();
@@ -15,29 +17,39 @@ function App() {
   return (
     <>
       <div className="app" id={contextTheme}>
-        <ReactSwitch
-          onChange={handleSwitch}
-          checked={checked}
-          onColor="#86d3ff"
-          onHandleColor="#2693e6"
-          handleDiameter={30}
-          uncheckedIcon={false}
-          checkedIcon={false}
-          boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-          activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-          height={20}
-          width={48}
-          className="react-switch"
-          id="material-switch"
-        />
+        <div className="app--landing__page__buttons--switch-container">
+          <ReactSwitch
+            onChange={handleSwitch}
+            checked={checked}
+            onColor="#86d3ff"
+            onHandleColor="#2693e6"
+            handleDiameter={30}
+            uncheckedIcon={false}
+            checkedIcon={false}
+            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+            height={20}
+            width={48}
+            className="react-switch"
+          />
+        </div>
         <main className="app--landing__page">
-          <img className="app--landing__page--logo" src={foto4} />
+          <img
+            className="app--landing__page--logo"
+            src={contextTheme === "Light" ? lightPizza : darkPizza}
+          />
         </main>
         <section className="app--landing__page__buttons">
-          <button className="app--landing__page__buttons--button" id={contextTheme}>
+          <button
+            className="app--landing__page__buttons--button"
+            id={contextTheme}
+          >
             LOG IN
           </button>
-          <button className="app--landing__page__buttons--button" id={contextTheme}>
+          <button
+            className="app--landing__page__buttons--button"
+            id={contextTheme}
+          >
             SIGN IN
           </button>
         </section>
