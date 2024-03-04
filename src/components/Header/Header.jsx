1791a5ccbe51c2b4a5a5995b572c2f87e6507474
Abián <70582564+abian22@users.noreply.github.com>
2024-03-04 13:37:21 +0000

    header for mobile screens added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,57 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { useThemeContext } from "../../context/ThemeContext";
 import { getProfile } from "../../services/user";
+import darkMenu from "../../assets/icons/darkMenu.svg";
+import lightMenu from "../../assets/icons/lightMenu.svg";
+import searchIcon from "../../assets/icons/search.svg";
 import "./Header.scss";
 
 function Header() {
   const navigate = useNavigate();
-  const [isMenuOpen, setMenuOpen] = useState(false);
+  const { contextTheme } = useThemeContext();
+  const [isMenuProfileOpen, setIsMenuProfileOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [profileImg, setProfileImg] = useState("");
 
   useEffect(() => {
     getProfileImg();
-  }, []);
+  },[] );
+
+  const toggleMenuProfile = () => {
+    setIsMenuProfileOpen(!isMenuProfileOpen);
+  };
 
   const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
+
+    setIsMenuOpen(!isMenuOpen);
   };
 
   const getProfileImg = async () => {
     const result = await getProfile();
     setProfileImg(result.profileImg);
   };
+
+  const menu = [
+    {
+      title: "HOME",
+      fun: () => {
+        navigate("/home");
+      },
+    },
+    {
+      title: "UPLOAD",
+      fun: () => {
+        // navigate("/login");
+      },
+    },
+    {
+      title: "CHAT",
+      fun: () => {
+        // navigate("/login");
+      },
+    },
+  ];
 
   const settings = [
     {
@@ -41,29 +73,63 @@ function Header() {
     <>
       <div className="headerContainer">
         <img
-          className="headerContainer__profileImg"
-          src={profileImg}
+          id={contextTheme}
+          className="headerContainer__menuIcon"
+          src={contextTheme === "Light" ? darkMenu : lightMenu}
           onClick={toggleMenu}
         />
-      </div>
-      {isMenuOpen && (
-        <div className="headerContainer__profilePannel">
-          <ul className="headerContainer__profilePannel--profileOptions">
-            {settings.map((item, index) => (
-              <li
-                className="headerContainer__profilePannel--profileOption"
-                key={index}
-                onClick={() => {
-                  toggleMenu();
-                  item.fun();
-                }}
-              >
-                {item.title}
-              </li>
-            ))}
-          </ul>
+        {isMenuOpen && (
+          <div className="menuContainer">
+            <ul className="menuContainer__menuUl">
+              {menu.map((option, index) => (
+                <li
+                  className="menuContainer__menuUl--menuLi"
+                  key={index}
+                  onClick={() => {
+                    toggleMenu();
+                    option.fun();
+                  }}
+                >
+                  {option.title}
+                </li>
+              ))}
+              <input
+                className="menuContainer__menuUl--searchInput"
+                placeholder="Buscar..."
+              />
+              <img
+                className="menuContainer__menuUl--searchIcon"
+                src={searchIcon}
+              />
+            </ul>
+          </div>
+        )}
+        <div className="headerContainer__profileContainer">
+          <img
+            className="headerContainer__profileImg"
+            src={profileImg}
+            onClick={toggleMenuProfile}
+          />
+          {isMenuProfileOpen && (
+            <div className="headerContainer__profilePannel">
+              <ul className="headerContainer__profilePannel--profileOptions">
+                {settings.map((item, index) => (
+                  <li
+                    className="headerContainer__profilePannel--profileOption"
+                    key={index}
+                    onClick={() => {
+                      toggleMenuProfile();
+                      item.fun();
+                    }}
+                  >
+                    {item.title}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </>
   );
 }
