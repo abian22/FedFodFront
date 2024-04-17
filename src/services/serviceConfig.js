fc15de905863fa6e07f1251a454980d43be678b9
Abián <70582564+abian22@users.noreply.github.com>
2024-04-17 12:32:09 +0100

    baseurl changed from glich to vercel

--- src/services/serviceConfig.js
+++ src/services/serviceConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"https://ttk-back2-git-main-abian22s-projects.vercel.app/api"
+    baseURL:"https://ttk-back2.vercel.app/api"
 })
 
 export default api
